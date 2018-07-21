ca7fa82141402093d0b7395186a2874b1d5e788c
naveen bansal <bansalnvn@gmail.com>
2018-07-21 14:46:44 +0800

    add credit_card_fraud dataset kernel

---
+++ credit_card_fraud/common/__init__.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import pandas as pd
+import cv2
+import glob
+
+_INPUT_FILE_NAME = "./data/creditcard.csv"
+
+
+def read_and_sanitize_data():
+    data = pd.read_csv(_INPUT_FILE_NAME, sep=',')
+    return data
+
+
+def read_input_data():
+    label_dictionary = {}
+    labels_all = os.listdir("./data/flowers/")
+    n_classes = len(labels_all)
+    print(n_classes)
+    print(labels_all)
+    for label in labels_all:
+        label_dictionary[label] = len(label_dictionary)
+    print(label_dictionary)
+    imgs = glob.glob("./data/flowers/*/*.jpg")
+    print(len(imgs))
+    x_train = []
+    y_train = []
+    x_train_rgb = []
+    for img in imgs:
+        y_train.append(label_dictionary[img.split('/')[-2]])
+        img_array = np.asarray(cv2.resize(cv2.imread(img, cv2.IMREAD_GRAYSCALE), (128, 128)))
+        img_array = img_array[..., np.newaxis]
+        x_train.append(img_array)
+        x_train_rgb.append(np.asarray(cv2.resize(cv2.imread(img), (128, 128))))
+    s = pd.Series(y_train)  # one hot encoding.
+    one_hot_encoded_y = pd.get_dummies(s)
+    # cv2.imshow("test_image", x_train[0])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return x_train, one_hot_encoded_y, n_classes, label_dictionary, np.asarray(x_train_rgb)
---
+++ credit_card_fraud/kernel.py
@@ -0,0 +1,34 @@
+from common import read_and_sanitize_data
+from keras.models import Sequential
+from keras.layers import Dense, Conv1D, Dropout, Flatten
+import numpy as np
+import tensorflow as tf
+from sklearn.utils import shuffle
+
+_BATCH_SIZE = 10132
+
+
+def model(input_shape):
+    print(input_shape)
+    model = Sequential()
+    model.add(Dense(32, input_shape=input_shape, activation='relu'))
+    model.add(Dense(1, activation='softmax'))
+    return model
+
+
+if __name__ == '__main__':
+    input_data = read_and_sanitize_data()
+    print(input_data.shape)
+    train_x = input_data.drop(labels='Class', axis=1)
+    print(train_x.shape)
+    train_y = input_data[['Class']]
+    print(train_y.shape)
+    train_x.drop(train_x.head(1).index, inplace=True)
+    train_y.drop(train_y.head(1).index, inplace=True)
+    model = model(train_x.shape[1:])
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    print(model.summary())
+    train_x, train_y = shuffle(train_x, train_y, random_state=2)
+    model.fit(np.asarray(train_x), np.asarray(train_y), epochs=100, batch_size=train_x.shape[0])
