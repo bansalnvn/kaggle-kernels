8b1b164c767bd8299b7984f5a21ff143766db0fd
naveen bansal <bansalnvn@gmail.com>
2018-07-23 20:27:15 +0800

    add initial graphs

--- fruit_and_vegetable_prices/common/__init__.py
+++ fruit_and_vegetable_prices/common/__init__.py
@@ -1,10 +1,23 @@
 import pandas as pd
+from pathlib import Path
+
+_DATA_STORE = "./data/pd.pickle"
 
 
 def read_input_data():
-    _csv_file_name = "./data/veg_and_fruits.csv"
-    df = pd.read_csv(_csv_file_name)
-    df = df[~pd.isnull(df['price'])]
-    df = df[df['price'] > 0.0]
-    print(df)
-    return df
+    # check if the _DATA_STORE file exist.
+    my_file = Path(_DATA_STORE)
+    if my_file.is_file():
+        df = pd.read_pickle(_DATA_STORE)
+    else:
+        today = pd.datetime.today()
+        _csv_file_name = "./data/veg_and_fruits.csv"
+        df = pd.read_csv(_csv_file_name)
+        df = df[~pd.isnull(df['price'])]
+        df = df[df['price'] > 0.0]
+        df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+        df = df[df['Date'].between('1980-01-01', today)]
+        df = df.filter(items=['Item Name', 'Date', 'price'])
+        pd.to_pickle(df, _DATA_STORE)
+    all_items = df['Item Name'].unique()
+    return df, all_items
--- fruit_and_vegetable_prices/kernel.py
+++ fruit_and_vegetable_prices/kernel.py
@@ -1,8 +1,22 @@
 from common import read_input_data
 import matplotlib.pyplot as plt
 
+
+def filter_crop(crop_name, dataframe):
+    filtered = dataframe[dataframe['Item Name'] == crop_name].filter(items=['Date', 'price'])
+    return filtered
+
+
 if __name__ == "__main__":
-    df = read_input_data()
-    print(df.size)
-    plt(df)
-    print('Hello World')
+    plot_index = 14
+    df, all_crops = read_input_data()
+    df.set_index('Date')
+    for idx, crop in enumerate(all_crops):
+        filtered_dataframe = filter_crop(crop, df)
+        print(filtered_dataframe.shape)
+        if filtered_dataframe.shape[0] > 2047:
+            print(idx, crop, filtered_dataframe)
+            plt.plot(filtered_dataframe['Date'], filtered_dataframe['price'], label=crop)
+            # filtered_dataframe.plot(x='Date', y='price', label=crop, marker='o')
+    plt.show(block=True)
+    plt.interactive(False)
